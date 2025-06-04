91689de96c85c2acf032c7ae7faa25957ac07703
Petr Laštovička <plastovicka@users.noreply.github.com>
2025-06-04 17:28:51 +0200

    F23 key in macro

--- source/Commands.cpp
+++ source/Commands.cpp
@@ -35,7 +35,7 @@ struct Tvk {
 	int vk;
 } vks[]={
 	{"ESCAPE", VK_ESCAPE}, {"ESC", VK_ESCAPE}, {"F10", VK_F10},
-	{"F11", VK_F11}, {"F12", VK_F12}, {"F1", VK_F1}, {"F2", VK_F2},
+	{"F11", VK_F11}, {"F12", VK_F12}, {"F23", VK_F23},{"F1", VK_F1}, {"F2", VK_F2},
 	{"F3", VK_F3}, {"F4", VK_F4}, {"F5", VK_F5}, {"F6", VK_F6}, {"F7", VK_F7},
 	{"F8", VK_F8}, {"F9", VK_F9}, {"PRINTSCREEN", VK_SNAPSHOT},
 	{"PRINTSCRN", VK_SNAPSHOT}, {"SCROLLLOCK", VK_SCROLL},
